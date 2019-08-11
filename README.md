ce1b80e0fee0f2369aaf038b5b9f33df99aca708
Vlad Afonin <afonin.com123@gmail.com>
2019-08-11 19:03:57 +0300

    many changes

--- README.md
+++ README.md
@@ -4,26 +4,45 @@ Simple lightweight JS keyboard hotkeys library
 ## Usage
 ```html
 <script src="hotkeys.js" type="text/javascript"></script>
+<script>
+let hotkeys = new Hotkeys
+
+hotkeys.set('ControlLeft+KeyS', () => {
+	event.returnValue = event.preventDefault()
+	console.log('Saved')
+})
+
+hotkeys.start()
+</script>
 ```
 
-### Start
+The Hotkeys object implements the standart js Map object
+<br>and it adds the following methods:
+
+### Configure
+```js
+hotkeys.configure()
+```
+Look at the console
+
+### Start listening
 ```js
 hotkeys.start()
 ```
 
-### Add new hotkey
+### Set new hotkey
 ```js
-hotkeys.add('alt+arrowleft', (hotkey) => {
-  alert(hotkey)
+hotkeys.set('AltLeft+ArrowRight', event => {
+	alert(event)
 })
 ```
 
-### Remove hotkey
+### Delete hotkey
 ```js
-hotkeys.remove('alt+arrowleft')
+hotkeys.delete('AltLeft+ArrowRight')
 ```
 
-### Stop
+### Stop listening
 ```js
 hotkeys.stop()
 ```
--- hotkeys.js
+++ hotkeys.js
@@ -1,37 +1,66 @@
 'use strict'
 
-let hotkeys = {
-	add: function(hotkey, callback) {
-		this.hotkeys[hotkey.toLowerCase()] = callback
-	},
-	remove: function(hotkey) {
-		delete this.hotkeys[hotkey.toLowerCase()]
-	},
-	keys: {},
-	hotkeys: {},
-	keydown: function(event) {
-		this.keys[event.key.toLowerCase()] = true
-		for(let hotkey in this.hotkeys) {
-			let comb = true
-			hotkey.split('+').forEach(key => {
-				if(!comb) return
-				if(!this.keys[key]) comb = false
-			})
-			if(comb) {
-				event.preventDefault()
-				this.hotkeys[hotkey](hotkey)
+class Hotkeys extends Map {
+	constructor() {
+		super()
+
+		this.downkeys = new Set()
+
+		this.keydown = event => {
+			this.downkeys.add(event.code)
+
+			nextHotkey:
+			for(let [hotkey, callback] of this) {
+				for(let key of hotkey)
+					if(!this.downkeys.has(key))
+						continue nextHotkey
+
+				callback(event)
 			}
 		}
-	},
-	keyup: function(event) {
-		this.keys[event.key.toLowerCase()] = false
-	},
-	start: function() {
-		document.addEventListener('keydown', this.keydown.bind(this))
-		document.addEventListener('keyup', this.keyup.bind(this))
-	},
-	stop: function() {
-		document.removeEventListener('keydown', this.keydown.bind(this))
-		document.removeEventListener('keyup', this.keyup.bind(this))
+
+		this.keyup = event => this.downkeys.delete(event.code)
+	}
+
+	set(hotkey, callback) {
+		super.set(hotkey.split('+'), callback)
+		return this
+	}
+
+	delete(hotkey) {
+		nextHotkey:
+		for(let [hkey] of this)
+			if(hotkey == hkey.join('+')) return super.delete(hkey)
+		return this
+	}
+
+	start() {
+		document.addEventListener('keydown', this.keydown)
+		document.addEventListener('keyup', this.keyup)
+		return this
+	}
+
+	stop() {
+		document.removeEventListener('keydown', this.keydown)
+		document.removeEventListener('keyup', this.keyup)
+		return this
+	}
+
+	configure() {
+		let keydown = event => {
+			event.returnValue = event.preventDefault()
+			this.downkeys.add(event.code)
+		}
+
+		let keyup = event => {
+			document.removeEventListener('keydown', keydown)
+			document.removeEventListener('keyup', keyup)
+			console.log(Array.from(this.downkeys.keys()).join('+'))
+			this.downkeys.clear()
+		}
+
+		document.addEventListener('keydown', keydown)
+		document.addEventListener('keyup', keyup)
+		console.log('now it is waiting for a combination')
 	}
 }
